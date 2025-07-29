c5ac96e042f58d4a89dc1b2bbf9514e93023bfd2
Jeudry <jeudrypp@gmail.com>
2025-07-29 11:39:35 -0400

    adjusted validations in backend for products

--- Backend/cmd/main/view_models/products/update_product.go
+++ Backend/cmd/main/view_models/products/update_product.go
@@ -1,12 +1,12 @@
 package products
 
 type UpdateProductPayload struct {
-	Name        string   `json:"name" validate:"required,max=128"`
-	Description *string  `json:"description,omitempty" validate:"omitempty,max=3000"`
-	Price       float64  `json:"price" validate:"gt=0,lt=10000000"`
-	RentalPrice *float64 `json:"rental_price,omitempty" validate:"omitempty,gt=0,lt=10000000"`
+	Name        string   `json:"name" validate:"required,min=3,max=256"`
+	Description *string  `json:"description,omitempty" validate:"omitempty,min=5,max=20000"`
+	Price       float64  `json:"price" validate:"gt=5,lt=10000000"`
+	RentalPrice *float64 `json:"rental_price,omitempty" validate:"omitempty,gt=5,lt=10000000"`
 	Color       uint64   `json:"color" validate:"gte=0,lte=4294967295"`
-	Size        float64  `json:"size"`
+	Size        float64  `json:"size" validate:"lte=99999"`
 	ImageURL    *string  `json:"image_url,omitempty" validate:"omitempty,max=3000"`
 	Stock       int      `json:"stock" validate:"max=100000000"`
 }
--- Backend/cmd/migrate/migrations/000016_add_products_table.up.sql
+++ Backend/cmd/migrate/migrations/000016_add_products_table.up.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS products (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    name TEXT NOT NULL,
-    description TEXT,
-    price DECIMAL(10, 2) NOT NULL,
-    rental_price DECIMAL(10, 2),
-    color BIGINT NOT NULL,
-    size DECIMAL(10, 2) NOT NULL,
-    image_url TEXT,
-    stock INT NOT NULL DEFAULT 0,
+    name TEXT NOT NULL CHECK (char_length(name) >= 3 AND char_length(name) <= 256),
+    description TEXT CHECK (description IS NULL OR (char_length(description) >= 5 AND char_length(description) <= 20000)),
+    price DECIMAL(10, 2) NOT NULL CHECK (price > 5 AND price < 10000000),
+    rental_price DECIMAL(10, 2) CHECK (rental_price IS NULL OR (rental_price > 5 AND rental_price < 10000000)),
+    color BIGINT NOT NULL CHECK (color >= 0 AND color <= 4294967295),
+    size DECIMAL(10, 2) NOT NULL CHECK (size <= 99999),
+    image_url TEXT CHECK (image_url IS NULL OR char_length(image_url) <= 3000),
+    stock INT NOT NULL DEFAULT 0 CHECK (stock <= 100000000),
     created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
